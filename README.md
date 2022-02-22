d1957f684de8bc2ec0b03768633369baada9ac4a
Larry Li <larryli@qq.com>
2022-02-22 17:24:11 +0800

    Init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # esp32-devkit-flasher
 ESP serial flasher for esp32 devikit
+
+## Hardware connection
+
+Table below shows connection between two ESP32 devices.
+
+| ESP32 (host) | ESP32 (slave) |
+|:------------:|:-------------:|
+|    IO26      |      IO0      |
+|    IO25      |    RESET/EN   |
+|    IO4       |      RX0      |
+|    IO5       |      TX0      |
---
+++ main/main.c
@@ -0,0 +1,19 @@
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+static const char *TAG = "main";
+
+void app_main(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    ESP_LOGI(TAG, "Init");
+}
